c97b3f088af3c86e27e50beb63bd70eac5e6a6be
ParkerBacall <pbacall2@gmail.com>
2019-12-11 09:04:29 -0700

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -2,6 +2,8 @@ class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
       t.timestamps null: false
+      t.references :taxi, foregin_key:true  
+      t.references :passenger, foregin_key:true  
     end
   end
 end
